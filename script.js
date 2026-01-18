bd678ef1f5bf3bffee954bb32c7accde821890ea
charliescontrollers <charliescontrollers@gmail.com>
2026-01-18 07:59:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -334,5 +334,4 @@ setInterval(loadVisibility, 600000);   // 10 min
 setInterval(loadNOTAMs, 1800000);      // 30 min
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.clouds.min.js"></script>
+
